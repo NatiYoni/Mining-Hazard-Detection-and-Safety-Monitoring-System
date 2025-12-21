d1e5360d98d9559e19a4ba29225ef21dbb5fc694
Natnael Yonas <yonasnatnael11@gmail.com>
2025-12-21 17:40:41 +0300

    feat: define domain entities and repository interfaces

---
+++ backend/domain/interfaces/repositories.go
@@ -0,0 +1,34 @@
+package interfaces
+
+import (
+	"minesense-backend/domain/entities"
+
+	"github.com/google/uuid"
+)
+
+type DeviceRepository interface {
+	Create(device *entities.Device) error
+	FindByID(id uuid.UUID) (*entities.Device, error)
+	FindAll() ([]entities.Device, error)
+}
+
+type SensorRepository interface {
+	Create(reading *entities.SensorReading) error
+	FindByDeviceID(deviceID uuid.UUID) ([]entities.SensorReading, error)
+}
+
+type AlertRepository interface {
+	Create(alert *entities.Alert) error
+	FindAll() ([]entities.Alert, error)
+	FindByDeviceID(deviceID uuid.UUID) ([]entities.Alert, error)
+}
+
+type UserRepository interface {
+	Create(user *entities.User) error
+	FindByUsername(username string) (*entities.User, error)
+}
+
+type ImageRepository interface {
+	Create(image *entities.Image) error
+	FindByID(id uuid.UUID) (*entities.Image, error)
+}
